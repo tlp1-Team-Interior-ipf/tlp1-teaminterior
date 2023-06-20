007fa1be41d2f7506cdf330be31fa69dc0bcf55d
Fabian <fabianvillagra28@gmail.com>
2023-06-20 14:59:45 -0300

    se agregaron las rutas del menu de choferes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,20 @@ const router = require('express').Router();
 
 router.get('/empresa', (req, res) => res.render('empresa'));
 
+router.get('/menu', (req, res) => res.render('menu'));
+
+router.get('/menu/cruz-del-norte', (req, res) => res.render('cruz-del-norte'));
+
+router.get('/menu/fontana', (req, res) => res.render('fontana'));
+
+router.get('/menu/libertad', (req, res) => res.render('libertad'));
+
+router.get('/menu/litur', (req, res) => res.render('litur'));
+
+router.get('/menu/monte-carlo', (req, res) => res.render('monte-carlo'));
+
+router.get('/menu/napoleon', (req, res) => res.render('napoleon'));
+
 router.get('/support', (req, res) => res.render('support'));
 
 router.get('/login', (req, res) => res.render('login'));
