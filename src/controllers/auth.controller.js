f47340292a630def77f1f03e26bdedfbc97b2246
Fabian <fabianvillagra28@gmail.com>
2023-06-30 17:08:32 -0300

    se agrego la verificacion en el inicio de sesion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,46 +1,42 @@
 const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuario');
-const { generarJWT } = require('../../helpers/generar_jwt');
+
 const authCtrl = {};
 
 authCtrl.login = async (req, res) => {
     const { email, contraseña } = req.body;
-
+  
     try {
-        // Verificar si el email existe
-        const existeUsuario = await Usuario.findOne({ email });
-        
-        if(!existeUsuario) {
-            return res.status(400).json({
-                message: 'El email no existe',
-            });
-        }
-
-
-        // Verificar la contraseña
-        const contraseñaValido = bcrypt.compareSync(contraseña, existeUsuario.contraseña);
-
-        if(!contraseñaValido) {
-            return res.status(400).json({
-                message: 'La contraseña no es válida',
-            });
-        }
-
-        // Generar el JWT
-        const token = await generarJWT(existeUsuario.id)
-
-        res.json({
-            message: 'Login correcto',
-            token,
+      // Verificar si el email existe
+      const existeUsuario = await Usuario.findOne({ where: { email } });
+  
+      if (!existeUsuario) {
+        return res.status(400).json({
+          message: 'El correo electrónico no está registrado',
         });
-
+      }
+  
+      // Verificar la contraseña
+      const contraseñaValida = await bcrypt.compare(contraseña, existeUsuario.contraseña);
+  
+      if (!contraseñaValida) {
+        return res.status(400).json({
+          message: 'La contraseña no es válida',
+        });
+      }
+  
+  
+      res.json({
+        message: 'Inicio de sesión correcto'
+      });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'Error al iniciar sesión',
-        });
+      console.log(error);
+      res.status(500).json({
+        message: 'Error al iniciar sesión',
+      });
     }
-}
+  };
+  
 
 
 
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 
 const usuarioCtrl = require('../controllers/usuario.controller');
 
-const { validarJWT } = require('../../middlewares/validar_jwt');
+const authCtrl = require('../controllers/auth.controller');
+
 
 router.get('/login/usuario', (req, res) => res.render('login/loginusuario'));
 
@@ -11,7 +12,7 @@ router.get('/login/usuario', (req, res) => res.render('login/loginusuario'));
 router.get('/registro/usuario', (req, res) => res.render('registro/signinusuario'));
 
 
-
+router.post('/login/usuario', authCtrl.login)
   
 router.post('/registro/usuario', usuarioCtrl.crearUsuario)
 
